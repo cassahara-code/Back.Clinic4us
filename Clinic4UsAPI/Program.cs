5b5733ac747f3582e1e845d53ef97b8a83b51b3b
fabio martins <fabioomartins@gmail.com>
2025-10-20 10:21:36 -0300

    Enhance AutoMapper and update project dependencies
    - Added new entity mappings in AutoMapperSetup.cs for creating and updating entities.
    - Updated Clinic4UsAPI.Web.csproj to include Entity Framework Core packages and added a project reference to Clinic4Us.Domain.csproj.
    - Refactored Program.cs to streamline DbContext configuration, rename CORS policy, and integrate AutoMapper setup into service registration.
    - Modified IoCExtensions.cs to register new services for IEntitiesService and IEntitiesRepository, and updated the method signature for RegistrarServicos.

--- Clinic4UsAPI/Program.cs
+++ Clinic4UsAPI/Program.cs
@@ -1,15 +1,7 @@
-using Application.Automapper;
-using Application.Commands.ViewModels;
-using Application.IRepositories;
-using Application.IServices;
-using Application.Services;
-using Application.Validators;
-using Clinic4Us.Data.Repositories;
 using Data.Context;
-using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
-using Application.DTOs.Requests;
+using Infra;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,79 +10,44 @@ builder.Configuration.AddUserSecrets<Program>();
 
 // Configura o DbContext para usar MySQL
 builder.Services.AddDbContext<Clinic4UsDbContext>(options =>
-    options.UseMySql(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        new MySqlServerVersion(new Version(8, 0, 36)), // Ajuste a versão conforme necessário
-        mySqlOptions => mySqlOptions.EnableStringComparisonTranslations()
-    )
-);
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseMySql(connectionString,
+        ServerVersion.Create(8, 0, 36, ServerType.MySql),
+        mySqlOptions => mySqlOptions.EnableRetryOnFailure()
+    );
+});
 
-// CORS
+// CORS: permite o front em http://localhost:3000
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("CorsPolicy", policy =>
+    options.AddPolicy("DefaultCors", policy =>
     {
-        policy
-            .AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader();
-        // Se precisar enviar cookies/credenciais, use SetIsOriginAllowed(_ => true).AllowCredentials()
-        // e substitua AllowAnyOrigin por uma lista explícita de origens.
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
     });
 });
 
-// Add services to the container.
-//IoCExtensions.RegistrarServicos(builder.Services);
+// Register all services using IoC Extensions
+builder.Services.RegistrarServicos();
 
-
-builder.Services.AddScoped<IPlanService, PlanService>();
-builder.Services.AddScoped<IPlansSubscriptionService, PlansSubscriptionService>();
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IUsersAddressService, UsersAddressService>();
-builder.Services.AddScoped<IPaymentRecurrenceService, PaymentRecurrenceService>();
-builder.Services.AddScoped<IMercadoPagoService, MercadoPagoService>();
-
-builder.Services.AddScoped<IPlanRepository, PlanRepository>();
-builder.Services.AddScoped<IPlansSubscriptionRepository, PlansSubscriptionRepository>();
-builder.Services.AddScoped<IUsersAddressRepository, UsersAddressRepository>();
-builder.Services.AddScoped<IPaymentRecurrenceRepository, PaymentRecurrenceRepository>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<ILandPageRepository, LandPageRepository>();
-
-// Registrando PlansBenefit
-builder.Services.AddScoped<IPlansBenefitRepository, PlansBenefitRepository>();
-builder.Services.AddScoped<IPlansBenefitService, PlansBenefitService>();
-builder.Services.AddScoped<IValidator<CreatePlansBenefitRequest>, CreatePlansBenefitRequestValidator>();
-builder.Services.AddScoped<IValidator<UpdatePlansBenefitRequest>, UpdatePlansBenefitRequestValidator>();
-
-// Registrando Benefits
-builder.Services.AddScoped<IBenefitsRepository, BenefitsRepository>();
-builder.Services.AddScoped<IBenefitsService, BenefitsService>();
-builder.Services.AddScoped<IValidator<CreateBenefitRequest>, CreateBenefitRequestValidator>();
-builder.Services.AddScoped<IValidator<UpdateBenefitRequest>, UpdateBenefitRequestValidator>();
-
-builder.Services.AddScoped<IValidator<PlanViewModel>, PlanViewModelValidator>();
-
+// ASP.NET Core Features
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddAutoMapper(cfg => cfg.AddProfile<AutoMapperSetup>());
-// Não é necessário AddHttpClient para SDK MercadoPago
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-//}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseDeveloperExceptionPage();
 app.UseHttpsRedirection();
 // Habilita CORS
 app.UseCors("DefaultCors");
-
-
-app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
